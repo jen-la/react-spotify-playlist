daf63a73625b4d77bed8a6fa25f319629065feee
Jennifer Lam <jlam86@gmail.com>
2020-12-21 16:19:41 +1100

    Update Playlist and SearchResults components

--- src/App.js
+++ src/App.js
@@ -29,6 +29,27 @@ class App extends React.Component {
           album: 'Unknown'
         }
       ],
+      playlistName: 'Jen\'s special mix',
+      playlistTracks: [
+        { 
+          id: 1,
+          name: 'Jet Plane', 
+          artist: 'Angus and Julia Stone', 
+          album: 'Unknown'
+        },
+        {
+          id: 2,
+          name: 'Circles', 
+          artist: 'Post Malone', 
+          album: 'Unknown'
+        },
+        {
+          id: 3,
+          name: 'Dynamite', 
+          artist: 'BTS', 
+          album: 'Unknown'
+        }
+      ],
     };
   }
   
@@ -43,6 +64,20 @@ class App extends React.Component {
   componentWillUnmount = () => {
     window.removeEventListener('resize', this.handleResize);
   };
+  
+  addTrack = (track) => {
+    if (!this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      // add song to playlist
+      const updatedPlaylist = [...this.state.playlistTracks, track];
+      // set new state of playlist
+      this.setState({ playlistTracks: updatedPlaylist });
+    } 
+  };
+
+  removeTrack = (track) => {
+    const filteredPlaylist = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id);
+    this.setState({ playlistTracks: filteredPlaylist });
+  };
 
   render = () => {
     return (
@@ -53,8 +88,8 @@ class App extends React.Component {
           {/* <ExamplePage viewportWidth={this.state.viewportWidth} /> */}
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div> 
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isRemoval: false,
-    };
-  }
-  
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isRemoval: this.props.isRemoval,
+  //   };
+  // }
   renderAction = () => {
-    return this.state.isRemoval ? '-' : '+';
+    return this.props.isRemoval === true ? <button className="Track-action" onClick={() => this.removeTrack}>-</button> : <button className="Track-action" onClick={() => this.addTrack}>+</button>;
   };
   
+  addTrack = () => {
+    this.props.onAdd(this.props.track);
+  };
+
+  removeTrack = () => {
+    this.props.onRemove(this.props.track);
+  };
+
   render = () => {
     return (
       <div className="Track" key={this.props.track.id}>
@@ -20,9 +27,9 @@ class Track extends React.Component {
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <button className="Track-action">{this.renderAction}</button>
+        {this.renderAction()}
       </div>
-      );
+    );
   };
 }
 
